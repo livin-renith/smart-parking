deea7627846972af557e4b825a61abef87eff0e4
livin-renith <livincfamily@gmail.com>
2026-04-19 12:44:37 +0530

    prepare backend for deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,12 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
+  host    : process.env.DB_HOST,
+  user    : process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port    : process.env.DB_PORT || 3306,
+  ssl     : { rejectUnauthorized: false }
 });
 
 db.connect((err) => {
